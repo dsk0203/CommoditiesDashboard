c1df5f6eb93e6c80d750f23f95611a16eaf047e9
dylan <dylan@example.com>
2021-04-29 22:30:34 -0400

    blog page looking fresh

--- src/mock-endpoints/mocks.js
+++ src/mock-endpoints/mocks.js
@@ -9,26 +9,56 @@ const mock = new MockAdapter(axios);
 mock.onGet('/api/data/economy_data').reply(200, require('./fixtures/economy_data.json'))
 
 // dashboard list endpoint
+mock.onGet('/api/blog-list').reply(200, 
+      [
+    {
+       title: "How I was able to break into data science",
+       date: '4/15/2015',
+       description: "Breaking into data science wasn't easy, but with these steps we are ....",
+       value_1: "value 1",
+       type_1: "type 1",
+       value_2: "value 2",
+       type_2: "type 2",
+       value_3: "type 3",
+       value_3: "value 3",
+       image_link: 'https://cdn.pixabay.com/photo/2020/09/16/11/48/donkeys-5576167_960_720.jpg'
+    },
+    {
+      title: "some_blog_post_2",
+      date: '4/21/2015',
+      description: "Org Repository Insights",
+      value_1: "value 1",
+      type_1: "type 1",
+      value_2: "value 2",
+      type_2: "type 2",
+      value_3: "type 3",
+      value_3: "value 3",
+      image_link: 'https://cdn.pixabay.com/photo/2021/04/21/13/20/cherry-blossoms-6196363_960_720.jpg'
+   }
+   ]
+                              )
+
+ // blog list endpoint
 mock.onGet('/api/dashboard-list').reply(200, [
-    {
-       path: "some_blog_post",
-       title: "Org Repository Insights",
-       description: "we are off to see the wizard, the wonderful wizard of oz"
-    },
-    {
-       path: "temp",
-       title: "how we lost our minds",
-       description: "omg"
-    },
-    {
-      path: "temp",
-      title: "how we lost our minds",
-      description: "omg"
+   {
+      path: "some_blog_post",
+      title: "Org Repository Insights",
+      description: "we are off to see the wizard, the wonderful wizard of oz"
    },
    {
       path: "temp",
       title: "how we lost our minds",
       description: "omg"
    },
+   {
+     path: "temp",
+     title: "how we lost our minds",
+     description: "omg"
+  },
+  {
+     path: "temp",
+     title: "how we lost our minds",
+     description: "omg"
+  },
 
- ])
+])
--- src/pages/BlogHome.jsx
+++ src/pages/BlogHome.jsx
@@ -1,42 +1,77 @@
-import React, {useEffect} from 'react'
-
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 
 export default function BlogHome({background}) {
 
+
+
+
+
+    // pass in whatever
+    const [blogList, setBlogList] = useState(undefined)
+    const [imageList, setImageList] = useState([])
+
     useEffect(() =>  {
 
-        document.body.style.backgroundImage = "url(" + "https://cdn.pixabay.com/photo/2017/06/21/07/33/background-2426328_960_720.jpg" + ")"
+        // background color
+        // document.body.style.backgroundImage = "url(" + "https://cdn.pixabay.com/photo/2017/06/21/07/33/background-2426328_960_720.jpg" + ")"
 
+        // axios get request
+        axios.get('/api/blog-list').then(({data}) => {
+            setBlogList(data)
+        })
+        
     },[])
 
 
     return (
-        <div className="cards-main-div">
-            <div class="card">
-                <div class="card-image"></div>
-                    <div class="card-text">
-                        <span class="date">4 days ago</span>
-                        <h2>Post One</h2>
-                        <p> lorem em sum dog</p>
-                    </div>
-                <div class="card-stats">
-                    <div className="stat">
-                        <div className="value">4m</div>
-                        <div className="type">read</div>
-                    </div>
+        <div className="do-nothing-div">
 
-                    <div className="stat border">
-                        <div className="value">5123</div>
-                        <div className="type">views</div>
-                    </div>
-
-                    <div className="stat">
-                        <div className="value">32</div>
-                        <div className="type">comments</div>
+            <div className="banner">
+                <div className="container">
+                    <div className="banner-content">
+                        <span>NEW ARRIVALS</span>
+                        <h1>Denim Jackets</h1>
+                        <p>lorem ipsum</p>
+                        <a href="#" className="btn-one">Discover</a>
+                        <a href="#" className="btn-two">Buy Now</a>
                     </div>
                 </div>
             </div>
-        </div>
+
+
+
+            <div className="display-flex-container">
+            {blogList && blogList.map(blog => (
+                <div className="cards-main-div">
+                    <div class="card">
+                        <div class="card-image" style ={ { backgroundImage: "url("+blog.image_link+")" } }></div>
+                            <div class="card-text">
+                                <span class="date">{blog.date}</span>
+                                <h2>{blog.title}</h2>
+                                <p>{blog.description}</p>
+                            </div>
+                        <div class="card-stats">
+                            <div className="stat">
+
+                                <div className="value">{blog.value_1}</div>
+                                <div className="type">{blog.type_1}</div>
+                            </div>
+
+                            <div className="stat border">
+                                <div className="value">{blog.value_2}</div>
+                                <div className="type">{blog.type_2}</div>
+                            </div>
+
+                            <div className="stat">
+                                <div className="value">{blog.value_3}</div>
+                                <div className="type">{blog.type_3}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>))}
+    </div>
+    </div>
     )
 }
